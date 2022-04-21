97ab9d44c2032a01d283b3b2b3bd5addfb0e80e7
Kazuki Mikami <kazuki.mikami@devenues.com>
2022-04-21 15:58:46 +0900

    ルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'App\Http\Controllers\HomeController@index');
+
+Route::get('/scream', 'App\Http\Controllers\ScreamController@index');
+
+Route::get('/register', 'App\Http\Controllers\RegisterController@index');
+
+Route::get('/login', 'App\Http\Controllers\LoginController@index');
+
+Route::get('/logout', 'App\Http\Controllers\LogoutController@index');
