4ec717419af68077ee6dbdd887b4bd368d2be273
Kazuki Mikami <kazuki.mikami@devenues.com>
2022-04-21 16:57:33 +0900

    インデント修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,13 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-      public function index()
-    {
+class HomeController extends Controller{
+    public function index() {
         return view('home');
     }
 }
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,13 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
-class LoginController extends Controller
-{
-      public function index()
-    {
+class LoginController extends Controller{
+    public function index() {
         return view('login');
     }
 }
